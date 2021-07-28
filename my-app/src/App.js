a25f5b8ffcc35ac22b1bf01d8be8d584b65c27e9
sajaabusafiah <sajakhalee595@gmail.com>
2021-07-29 01:36:59 +0300

    requested changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,24 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Header from './components/Header';
-import ContentList from './components/ContentList';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import ContentList from "./components/ContentList";
+require("dotenv").config();
 function App() {
- const [content, setContent] = useState()
-  useEffect( () => {
-    fetch(' https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=6ee32035bd5e430b923efffa01bdfc51')
-    .then(data => data.json())
-    .then( res => setContent(res.articles) )
-  }, [])
+  const [content, setContent] = useState();
+  useEffect(() => {
+    const apikey = process.env.REACT_APP_UNSPLASH_KEY;
+    fetch(
+      `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apikey}`
+    )
+      .then((data) => data.json())
+      .then((res) => setContent(res.articles));
+  }, []);
 
   return (
-
     <div className="App">
-      
-      <Header/>
-      <div className="content">  
-       <ContentList content={content} /> 
+      <Header />
+      <div className="content">
+        <ContentList content={content} />
       </div>
     </div>
   );
--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import '../App.css';
 const Header = () => {
     return (
         <div className="Header">
-           <h2> BBC NEWS </h2> 
+            <h2> BBC NEWS </h2>
         </div>
     )
 }
